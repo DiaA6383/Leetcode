8b7e54b4ddbf9ff94bddf1d8fb0f24f1ac8c84ae
Alex <DiaA6383@gmail.com>
2022-12-25 22:56:25 -0500

    first thing

--- main.cpp
+++ main.cpp
@@ -6,4 +6,11 @@
 
 int main() {
 
+    Solution test;
+    std::string test_string = "Hello, there!";
+    std::vector<int> test_indices = {12, 11, 10, 9, 8 ,7 , 6, 5 ,4 ,3 ,2 ,1 ,0}
+
+    std::cout << test.restoreString(test_string, test_indices);
+
+
 }
